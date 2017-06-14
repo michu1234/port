2617a3e4061f57ac542ce42c7287cd4dcebe1a5a
michu1234 <moooderator@gmail.com>
2017-06-14 08:04:02 +0200

    Newest version

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
 	$(".load-container").delay(5000).slideUp("slow");
+	$("hr").fadeOut(6100, function(){
+		$(this).remove();
+	})
 	$(".container").show("fast");
 	$(function() {
 		$(".info1").addClass("animateinfo").siblings().removeClass("animateinfo animateinfo2 offacc imgdown");
